b71b0baa4ef132face9c481f2ab65a7dd5b432fe
kichkiro <kichkiro@student.42firenze.it>
2024-04-27 20:43:40 +0200

    update CGI

--- project/src/cgi/Cgi.cpp
+++ project/src/cgi/Cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: kichkiro <kichkiro@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 04:12:07 by kichkiro          #+#    #+#             */
-/*   Updated: 2024/04/26 14:09:35 by kichkiro         ###   ########.fr       */
+/*   Updated: 2024/04/27 20:41:57 by kichkiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,29 @@
 #include "Cgi.hpp"
 
 Cgi::Cgi(Request &req, std::string path_info) {
-    this->_params["SCRIPT_FILENAME"] = "";
-    this->_params["QUERY_STRING"] = req.requestBody;
+    this->_params["SCRIPT_FILENAME"] = "/home/kichkiro/42/webserv/project/var/www/cgi-bin/hello.py"; // Parse
+    this->_params["QUERY_STRING"] = ""; // TODO
     this->_params["REQUEST_METHOD"] = req.request["method"];
     this->_params["CONTENT_TYPE"] = req.requestHeaders["Content-Type"];
     this->_params["CONTENT_LENGTH"] = req.requestHeaders["Content-Length"];
-    this->_params["SCRIPT_NAME"] = "";
-    this->_params["REQUEST_URI"] = "";
-    this->_params["DOCUMENT_URI"] = "";
+    this->_params["SCRIPT_NAME"] = ""; // TODO
+    this->_params["REQUEST_URI"] = ""; // TODO
+    this->_params["DOCUMENT_URI"] = ""; // TODO
     this->_params["DOCUMENT_ROOT"] = path_info;
     this->_params["SERVER_PROTOCOL"] = "HTTP/1.1";
     this->_params["REQUEST_SCHEME"] = "http";
     this->_params["HTTPS"] = "off";
     this->_params["GATEWAY_INTERF"] = "CGI/1.1";
     this->_params["SERVER_SOFTWARE"] = "webserv/1.0";
-    this->_params["REMOTE_ADDR"] = "";
-    this->_params["REMOTE_PORT"] = "";
-    this->_params["REMOTE_USER"] = "";
-    this->_params["SERVER_ADDR"] = "";
-    this->_params["SERVER_PORT"] = "";
-    this->_params["SERVER_NAME"] = "";
+    this->_params["REMOTE_ADDR"] = ""; // TODO
+    this->_params["REMOTE_PORT"] = ""; // TODO
+    this->_params["REMOTE_USER"] = ""; // TODO
+    this->_params["SERVER_ADDR"] = ""; // TODO
+    this->_params["SERVER_PORT"] = ""; // TODO
+    this->_params["SERVER_NAME"] = ""; // TODO
 
-    std::cout << "DEBUG CGI:" << this->_params["REQUEST_METHOD"] << std::endl;
+    // std::cout << "DEBUG CGI:" << this->_params["SCRIPT_FILENAME"] << std::endl;
+    this->exec();
 }
 
 Cgi::~Cgi(void) {}
@@ -50,8 +51,13 @@ std::string Cgi::exec(void) {
     char *const argv[] = {NULL};
     char *envp[this->_params.size() + 1];
     int i = 0;
-    
-    for (std::map<std::string, std::string>::iterator iter = _params.begin(); iter != _params.end(); ++iter) {
+    char buffer[4096];
+    std::string result;
+    ssize_t bytesRead;
+    int status;
+
+    for (std::map<std::string, std::string>::iterator iter = _params.begin();
+         iter != _params.end(); ++iter) {
         std::string env_str = iter->first + "=" + iter->second;
         envp[i] = strdup(env_str.c_str());
         i++;
@@ -70,29 +76,29 @@ std::string Cgi::exec(void) {
         return "";
     }
     else if (!pid) {
-        close(pipe_fds[1]);
-
-        if (dup2(pipe_fds[0], STDIN_FILENO) == -1) {
+        close(pipe_fds[0]);
+        if (dup2(pipe_fds[1], STDOUT_FILENO) == -1 || dup2(pipe_fds[1], STDERR_FILENO) == -1) {
             Log::error("CGI: Error duplicating pipe file descriptor");
             return "";
         }
-
-        close(pipe_fds[0]);
-
-        // if (execve(this->_params["SCRIPT_FILENAME"], argv, envp) == -1) {
-        //     Log::error("CGI: execve failed");
-        //     return "";
-        // }
+        close(pipe_fds[1]);
+        if (execve(this->_params["SCRIPT_FILENAME"].c_str(), argv, envp) == -1) {
+            exit(EXIT_FAILURE);
+        }
     }
     else {
-        close(pipe_fds[0]);
-        if (write(pipe_fds[1], this->_params["QUERY_STRING"].c_str(), 
-                  this->_params["QUERY_STRING"].size()) == -1) {
-            Log::error("CGI: Error writing to pipe");
+        close(pipe_fds[1]);
+        while ((bytesRead = read(pipe_fds[0], buffer, sizeof(buffer))) > 0) {
+            result.append(buffer, bytesRead);
+        }
+        if (bytesRead == -1) {
+            Log::error("CGI: Error reading from pipe");
             return "";
         }
-        close(pipe_fds[1]);
-        int status;
+        close(pipe_fds[0]);
         waitpid(pid, &status, 0);
+        std::cout << "---BEGIN RESULT---" << std::endl << result << "---END RESULT---"<< std::endl;
+        return result;
     }
+    return "";
 }
